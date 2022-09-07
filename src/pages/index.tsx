10c394ddd6a72728622aafcf0e7f21c032ffd87d
ilyasudakov <ilyasudakov.dev@gmail.com>
2022-09-07 10:51:34 +0300

    Update Card design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { CarType, getCarsByBrand } from "../utils/API/carBrands";
 import CarList, { CAR_BRANDS } from "../widgets/CarList";
 
 const Home = ({ list }: PageProps) => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Maximum</title>
         <meta name="description" content="Maximum тестовое задание" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <CarList list={list} />
-    </div>
+    </>
   );
 };
 
